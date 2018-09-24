d1c5e5c61011dc6a1e242db0303b1beb7e18c032
T_AndreL1 <andre.leme@verifone.com>
2018-09-24 17:48:54 -0300

    Add bootstrap
    seção 3 - Aula 30 = ok

---
+++ Fullstack Web API e Javascript/UI/scripts/script.js
@@ -0,0 +1,120 @@
+	var tbody = document.querySelector('table tbody');
+	var aluno = {};
+
+	function Cadastrar() 
+	{
+		aluno.nome = document.querySelector('#nome').value;
+		aluno.sobrenome = document.querySelector('#sobrenome').value;
+		aluno.telefone = document.querySelector('#telefone').value;
+		aluno.ra = document.querySelector('#ra').value;
+
+		console.log(aluno);
+
+		if(aluno.id === undefined || aluno.id === 0)
+		{
+			salvarEstudantes('POST', 0, aluno);
+		}
+		else
+		{
+			salvarEstudantes('PUT', aluno.id, aluno);
+		}
+		carregaEstudantes();
+	}
+
+	function Cancelar()
+	{
+		var btnSalvar = document.querySelector('#btnSalvar');
+		var btnCancelar = document.querySelector('#btnCancelar');
+		var titulo = document.querySelector('#titulo');
+
+		document.querySelector('#nome').value = '';
+		document.querySelector('#sobrenome').value = '';
+		document.querySelector('#telefone').value = '';
+		document.querySelector('#ra').value = '';
+
+		aluno = {};
+
+		btnSalvar.textContent = 'Cadastrar';
+		btnCancelar.textContent = 'Limpar';
+
+		titulo.textContent = 'Cadastrar Aluno'
+	}
+
+	function carregaEstudantes() {
+		tbody.innerHTML = ''
+		var xhr = new XMLHttpRequest();
+		xhr.open(`GET`, `http://localhost:50221/api/Aluno`, true);
+
+		xhr.onload = function () {
+			var estudantes = JSON.parse(this.responseText);
+			for(var indice in estudantes){
+				adicionaLinha(estudantes[indice]);
+			}
+		}
+
+		xhr.send();
+	}
+
+	function salvarEstudantes(metodo, id, corpo)
+	{
+		var xhr = new XMLHttpRequest();
+
+		if(id === undefined || id === 0)
+			id = '';
+
+		xhr.open(metodo, `http://localhost:50221/api/Aluno/${id}`, false);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(corpo));
+	}
+
+	function excluirEstudante(id)
+	{
+		var xhr = new XMLHttpRequest();
+
+		xhr.open('DELETE', `http://localhost:50221/api/Aluno/${id}`, false);
+
+		xhr.send();
+	}
+
+	function excluir(id)
+	{
+		excluirEstudante(id);
+		carregaEstudantes();
+	}
+
+	carregaEstudantes('GET');
+
+	function editarEstudante(estudante){
+		var btnSalvar = document.querySelector('#btnSalvar');
+		var btnCancelar = document.querySelector('#btnCancelar');
+		var titulo = document.querySelector('#titulo');
+
+		document.querySelector('#nome').value = estudante.nome;
+		document.querySelector('#sobrenome').value = estudante.sobrenome;
+		document.querySelector('#telefone').value = estudante.telefone;
+		document.querySelector('#ra').value = estudante.ra;
+
+		btnSalvar.textContent = 'Salvar';
+		btnCancelar.textContent = 'Cancelar';
+
+		titulo.textContent = `Editar Aluno ${estudante.nome}`;
+
+		aluno = estudante;
+
+		console.log(estudante);
+	}
+
+	function adicionaLinha(estudante) {
+		var trow = `<tr>
+						<td>${estudante.nome}</td>
+						<td>${estudante.sobrenome}</td>
+						<td>${estudante.telefone}</td>
+						<td>${estudante.ra}</td>
+						<td>
+							<button onclick='editarEstudante(${JSON.stringify(estudante)})'>Editar</button>
+							<button onclick='excluir(${estudante.id})'>Excluir</button>
+						</td>
+				    </tr>`
+				   
+		tbody.innerHTML += trow;
+	}
