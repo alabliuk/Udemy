a54875e343d5dcb6a4e493c9fa047bbd2bac796b
T_AndreL1 <andre.leme@verifone.com>
2018-10-04 11:25:01 -0300

    Finalização aula 7

---
+++ Criando Apis robustas em C#/XGame/XGame.AppConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using XGame.Domain.Argumets.Jogador;
+using XGame.Domain.Services;
+
+namespace XGame.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando ...");
+
+            var service = new ServiceJogador();
+            Console.WriteLine("Criei instancia do servico");
+
+            AutenticarJogadorRequest request = new AutenticarJogadorRequest();
+            Console.WriteLine("Criei instancia do meu objeto request");
+
+            var response = service.AutenticarJogador(request);
+
+            Console.ReadKey();           
+
+        }
+    }
+}
