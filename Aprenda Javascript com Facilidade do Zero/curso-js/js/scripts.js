fa9bab954ae6ecc64d9ff7f857f1387997c0006c
T_AndreL1 <andre.leme@verifone.com>
2018-10-08 18:50:51 -0300

    4. Tipos de dados primitivos - Strings

--- Aprenda Javascript com Facilidade do Zero/curso-js/js/scripts.js
+++ Aprenda Javascript com Facilidade do Zero/curso-js/js/scripts.js
@@ -1,11 +1,27 @@
+// ATALHOS
+// Comando para comentar linha no VS Code => CTRL + ;
+
+// AULA 02
 // alert("Mensagem enviada pelo Alert");
 // console.log("Mensagem enviada pelo console.log");
 
-// Comando para comentar linha no VS Code => CTRL + ;
+// AULA 3
+// var latitude = 40.87663;
+// var longitude = -8.08373;
+// console.log(latitude);
+// console.log(longitude);
 
+// AULA 4
+var nome = "João";  // podem ser usadas aspas duplas
+var sobrenome = 'Gomes';  // ou aspas simples
+var cpf = '111.111.111-11';
+var telefone = "998887655";
+var ddd = '21';
+var email = 'joao@gmail.com';
+var biografia = "João começou a sua carreira como desenvolvedor web em 2010 e já participou de importantes projetos, como..."; ''
 
-var latitude = 40.87663;
-var longitude = -8.08373;
-
-console.log(latitude);
-console.log(longitude);
+console.log('Nome completo: ' + nome + ' ' + sobrenome);
+console.log('(' + ddd + ')' + telefone);
+console.log('Primeira letra do nome: ' + nome[0]);
+console.log('Última letra do nome: ' + nome[3]);
+console.log('Número de caracteres do telefone: ' + telefone.length);
