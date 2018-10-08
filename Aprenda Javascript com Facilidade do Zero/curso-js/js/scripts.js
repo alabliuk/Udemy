05603b510f611dc7d0dd6d2b2e66d50af497d57a
T_AndreL1 <andre.leme@verifone.com>
2018-10-08 15:07:36 -0300

    3. Variáveis

--- Aprenda Javascript com Facilidade do Zero/curso-js/js/scripts.js
+++ Aprenda Javascript com Facilidade do Zero/curso-js/js/scripts.js
@@ -1,5 +1,11 @@
-alert("Mensagem enviada pelo Alert");
-
-console.log("Mensagem enviada pelo console.log");
+// alert("Mensagem enviada pelo Alert");
+// console.log("Mensagem enviada pelo console.log");
 
 // Comando para comentar linha no VS Code => CTRL + ;
+
+
+var latitude = 40.87663;
+var longitude = -8.08373;
+
+console.log(latitude);
+console.log(longitude);
