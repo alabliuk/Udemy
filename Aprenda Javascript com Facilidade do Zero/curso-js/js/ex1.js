f7bc68236c212c1bbab3679fff3a73560f2aba82
T_AndreL1 <andre.leme@verifone.com>
2018-10-09 17:24:16 -0300

    8. [FIX] Folha de Exercícios n. 1

--- Aprenda Javascript com Facilidade do Zero/curso-js/js/ex1.js
+++ Aprenda Javascript com Facilidade do Zero/curso-js/js/ex1.js
@@ -2,11 +2,14 @@
 console.log('Exercicio 01 - OK');
 
 //Ex02
+console.log('==================================================');
 var nome = "Andre";
-var idade = 26;
-console.log("Olá, eu me chamo " + nome + ", tenho " + idade + " anos e estou estudando Javascript")
+var anoNasc = 1992;
+var anoAtual = 2018
+console.log("Olá, eu me chamo " + nome + ", tenho " + (anoAtual - anoNasc) + " anos e estou estudando Javascript")
 
 //Ex03
+console.log('==================================================');
 var nome = "Andre";
 var matricula = "123";
 var prova1 = 10;
@@ -14,6 +17,7 @@ var prova2 = 8;
 console.log("O aluno " + nome + ", matrícula " + matricula + ", obteve a média final " + ((prova1 + prova2) / 2));
 
 //Ex04
+console.log('==================================================');
 var cel1 = "114141225";
 var cel2 = "4774918";
 var result1 = false
@@ -30,20 +34,22 @@ if(cel2.length == 9){
 console.log("Resultado do teste: " + result2)
 
 //Ex05
+console.log('==================================================');
 console.log(Math.pow(32, 6));
 
 //Ex06
 /*
 a) var NOME;  ==  OK
-b) var $num1;var 2a_tentativa; ==  INVALIDO
+b) var $num1;; ==  OK
 c) var typeof;  ==  INVALIDO
-d) var nome-aluno;  ==  OK
+d) var nome-aluno;  ==  INVALIDO
 e) var tentativa_2;  ==  OK
 f) var 2a_tentativa; ==  INVALIDO
 g) var nome completo; ==  INVALIDO
 */
 
 //Ex07
+console.log('==================================================');
 var quantidade = "25";
 var numero = 6;
 var pressao;
@@ -58,11 +64,12 @@ console.log(typeof pressao);
 console.log(typeof temperatura);
 
 //Ex08
+console.log('==================================================');
 var idade = 65;
 
 console.log(idade != 65);//false
 console.log(idade >= 65); //true
-console.log("65" == idade);
+console.log("65" == idade);//true
 console.log(65 !== idade);//false
 console.log(typeof (idade > 60));
 console.log(typeof (idade > 70));
