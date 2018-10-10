7dc7bb9c81fa7765e164b579588d6bdb77cd8902
T_AndreL1 <andre.leme@verifone.com>
2018-10-10 12:17:58 -0300

    11. Trabalhando com o DOM

--- Aprenda Javascript com Facilidade do Zero/curso-js/js/scripts.js
+++ Aprenda Javascript com Facilidade do Zero/curso-js/js/scripts.js
@@ -30,72 +30,80 @@
 
 //----------------------------------------------------------------
 //AULA 5
-var num1 = 20;
-var num2 = 3;
+// var num1 = 20;
+// var num2 = 3;
+// var soma = num1 + num2;
+// var subtracao = num1 - num2;
+// var divisao = num1 / num2;
+// var multiplicacao = num1 * num2;
+// var media = (num1 + num2) / 2;
 
-var soma = num1 + num2;
-var subtracao = num1 - num2;
-var divisao = num1 / num2;
-var multiplicacao = num1 * num2;
-
-var media = (num1 + num2) / 2;
-console.log('A media é: ' + media);
-console.log(Math.pow(media, 4));
+// console.log('A media é: ' + media);
+// console.log(Math.pow(media, 4));
 
 /* Parênteses podem ser usados para montar expressões. Neste caso eles são necessários para calcular a média de dois números, 
 caso contrário a divisão num2 / 2 seria realizada antes da soma, devido à ordem das operações matemáticas. */  
 
-var increment = 20;
-increment = increment + 5; // redefinimos o valor dela, para o valor que ela já possuía, mais 5.
-console.log(increment); // O console mostrará 25
+// var increment = 20;
+// increment = increment + 5; // redefinimos o valor dela, para o valor que ela já possuía, mais 5.
+// console.log(increment); // O console mostrará 25
 
-// esta estrutura variável = variável + valor, pode ser simplificada com o operador incremental +=
+// // esta estrutura variável = variável + valor, pode ser simplificada com o operador incremental +=
 
-increment = 1;
-increment += 5;
-console.log(increment); // O console mostrará 6
+// increment = 1;
+// increment += 5;
+// console.log(increment); // O console mostrará 6
 
-// podemos também usar *=, /= e -= para multiplicação, divisão e subtração
+// // podemos também usar *=, /= e -= para multiplicação, divisão e subtração
 
-var ddd = 21;
-var telefone = 998887655;
+// var ddd = 21;
+// var telefone = 998887655;
 
-/* Se quisermos montar o telefone completo usando concatenação, podemos ter problemas já que as variáveis são do tipo número. 
-Neste caso devemos convertê-las antes de montar o telefone. 
-Para isso, usamos o método toString, que recebe entre parênteses a valor ou variável que queremos converter.*/
+// /* Se quisermos montar o telefone completo usando concatenação, podemos ter problemas já que as variáveis são do tipo número. 
+// Neste caso devemos convertê-las antes de montar o telefone. 
+// Para isso, usamos o método toString, que recebe entre parênteses a valor ou variável que queremos converter.*/
 
-var ddd_string = ddd.toString();
-var tel_string = telefone.toString();
+// var ddd_string = ddd.toString();
+// var tel_string = telefone.toString();
+// var telefone_completo = ddd_string + tel_string;
+// console.log(telefone_completo); // retorna '21998887655'
 
-var telefone_completo = ddd_string + tel_string;
+// var idade = '17';
+// var idade_num = parseInt(idade);
+// idade_num++;
 
-console.log(telefone_completo); // retorna '21998887655'
-
-var idade = '17';
-var idade_num = parseInt(idade);
-idade_num++;
-
-console.log(idade_num); // O console mostrará 18
+// console.log(idade_num); // O console mostrará 18
 
 
-//----------------------------------------------------------------
-// AULA 6
+// //----------------------------------------------------------------
+// // AULA 6
 
-var v_string = "teste";
-var v_numero = 4.55;
-var idade = 21;
-var v_boolean = idade > 18 ;
+// var v_string = "teste";
+// var v_numero = 4.55;
+// var idade = 21;
+// var v_boolean = idade > 18 ;
+// console.log(typeof v_string);  // retorna string
+// console.log(typeof v_numero);  // retorna number
+// console.log(typeof v_boolean);  // retorna boolean
 
-console.log(typeof v_string);  // retorna string
-console.log(typeof v_numero);  // retorna number
-console.log(typeof v_boolean);  // retorna boolean
+
+// //----------------------------------------------------------------
+// // AULA 7
+// var undf = undefined;
+// var nl = null;
+// console.log(nl === undf);  // O console mostrará false
+// console.log(typeof undf);  // O console mostrará undefined
+// console.log(typeof nl);  // O console mostrará object
 
 
-//----------------------------------------------------------------
-// AULA 7
-var undf = undefined;
-var nl = null;
-console.log(nl === undf);  // O console mostrará false
+// // //----------------------------------------------------------------
+// // // AULA 10
+// //Pegando conteudo da caixa azul
+// var conteudo_caixa = document.getElementById("caixa_azul").innerHTML;
+// console.log(conteudo_caixa);
 
-console.log(typeof undf);  // O console mostrará undefined
-console.log(typeof nl);  // O console mostrará object
+// //Atribuindo conteudo da caixa azul na caixa amarela
+// document.getElementById("caixa_amarela").innerHTML = conteudo_caixa;
+
+document.getElementById("caixa_amarela").innerHTML = '<h1>' + 'caixa amarela' + '</h1>';
+document.getElementById("caixa_azul").innerHTML = '<h1>' + document.getElementById("caixa_azul").innerHTML + '</h1>';
