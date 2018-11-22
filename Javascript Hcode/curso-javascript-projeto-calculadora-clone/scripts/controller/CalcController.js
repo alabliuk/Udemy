72a3365be38f88351695002c1c7bbf0bd00352c8
T_AndreL1 <andre.leme@verifone.com>
2018-11-22 19:42:05 -0200

    C20 - Definir o valor máximo

--- Javascript Hcode/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
+++ Javascript Hcode/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -371,6 +371,10 @@ class CalcController {
     }
 
     set displayCalc(value) {
+        if (value.toString().length > 10) {
+            this.setError();
+            return false
+        }
         this._displayCalcE1.innerHTML = value;
     }
 
