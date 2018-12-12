bfb536cc597aa188048a34af87ee3933973cce88
T_AndreL1 <andre.leme@verifone.com>
2018-12-12 19:04:57 -0200

    G7 - Trabalhando com Json

--- Javascript Hcode/curso-javascript-projeto-usuarios/index.js
+++ Javascript Hcode/curso-javascript-projeto-usuarios/index.js
@@ -1,22 +1,15 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var pasword = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("exampleInputAdmin");
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {}; 
 
 fields.forEach(function (field, index) {
 
     if (field.name == "gender") {
         if (field.checked) {
-
+            user[field.name] = field.name;
         }
+    } else {
+        user[field.name] = field.name;
     }
-
-    console.log(field);
-
 });
+
+console.log(user);
