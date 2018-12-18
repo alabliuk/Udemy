48aca854245d4cf80866d9636b59efef57cc5997
T_AndreL1 <andre.leme@verifone.com>
2018-12-18 18:07:43 -0200

    G8 - Trabalhando com Eventos no JavaScript

--- Javascript Hcode/curso-javascript-projeto-usuarios/index.js
+++ Javascript Hcode/curso-javascript-projeto-usuarios/index.js
@@ -1,15 +1,26 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
-var user = {}; 
+var user = {};
 
-fields.forEach(function (field, index) {
+// document.querySelectorAll("button").forEach(function () {
+//     this.addEventListener("click", function () {
+//         console.log("clicou");
+//     });
+// });
 
-    if (field.name == "gender") {
-        if (field.checked) {
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+
+        if (field.name == "gender") {
+            if (field.checked) {
+                user[field.name] = field.name;
+            }
+        } else {
             user[field.name] = field.name;
         }
-    } else {
-        user[field.name] = field.name;
-    }
-});
+    });
 
-console.log(user);
+    console.log(user);
+
+});
