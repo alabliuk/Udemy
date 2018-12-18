fb509cfe5dce98376e3ca101e2271f85eac7a8b1
T_AndreL1 <andre.leme@verifone.com>
2018-12-18 18:49:01 -0200

    G9 - Trabalhando com Funções

--- Javascript Hcode/curso-javascript-projeto-usuarios/index.js
+++ Javascript Hcode/curso-javascript-projeto-usuarios/index.js
@@ -1,11 +1,25 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-// document.querySelectorAll("button").forEach(function () {
-//     this.addEventListener("click", function () {
-//         console.log("clicou");
-//     });
-// });
+function addLine(dataUser) {
+    var tr = document.createElement("tr");
+    console.log("addline", dataUser);
+
+    tr.innerHTML = `
+                    <tr>
+                      <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                      <td>${dataUser.name}</td>
+                      <td>${dataUser.email}</td>
+                      <td>${dataUser.admin}</td>
+                      <td>${dataUser.birth}</td>
+                      <td>
+                        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                      </td>
+                    </tr>
+        `;
+    document.getElementById("table-users").appendChild(tr);
+}
 
 document.getElementById("form-user-create").addEventListener("submit", function (event) {
     event.preventDefault();
@@ -14,13 +28,13 @@ document.getElementById("form-user-create").addEventListener("submit", function 
 
         if (field.name == "gender") {
             if (field.checked) {
-                user[field.name] = field.name;
+                user[field.name] = field.value;
             }
         } else {
-            user[field.name] = field.name;
+            user[field.name] = field.value;
         }
     });
 
-    console.log(user);
+    addLine(user);
 
 });
