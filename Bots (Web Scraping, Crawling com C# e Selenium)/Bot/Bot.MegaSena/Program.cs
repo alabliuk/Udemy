2f6dbaefa7d9e33e1ed1bfee3a1b6d55b815475b
T_AndreL1 <andre.leme@verifone.com>
2018-12-20 16:17:29 -0200

    Robô para obter resultado da MegaSena usando html

---
+++ Bots (Web Scraping, Crawling com C# e Selenium)/Bot/Bot.MegaSena/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Bot.MegaSena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = @"https://www.google.com/search?q=megasena";
+            string html;
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers["Cookie"] = "security=true";
+
+                //Baixa todo o conteudo da pagina(URL)
+                html = wc.DownloadString(url);
+            }
+
+            Console.WriteLine(html);
+
+            Console.ReadKey();
+        }
+    }
+}
