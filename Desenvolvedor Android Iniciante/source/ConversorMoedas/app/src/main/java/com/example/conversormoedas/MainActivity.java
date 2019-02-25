b0424089a33f142faa0655a168f5356386cca50b
T_AndreL1 <andre.leme@verifone.com>
2019-02-25 16:49:42 -0300

    Adicionando eventos aos elementos

--- Desenvolvedor Android Iniciante/source/ConversorMoedas/app/src/main/java/com/example/conversormoedas/MainActivity.java
+++ Desenvolvedor Android Iniciante/source/ConversorMoedas/app/src/main/java/com/example/conversormoedas/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -19,7 +20,19 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textDollar = (TextView) findViewById(R.id.text_dollar);
         this.mViewHolder.textEuro = (TextView) findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = (Button) findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.button_calculate){
+            //logica botao
+            this.mViewHolder.buttonCalculate.setText("TESTE");
+        }
+    }
+
 
     private static class ViewHolder{
         EditText editValue;
